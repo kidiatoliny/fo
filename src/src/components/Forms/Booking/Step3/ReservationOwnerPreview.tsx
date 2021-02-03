00b82ea62e5045a53eb022bf35b1e44a5973575f
kid <kidiatoliny@gmail.com>
2021-02-03 14:27:14 -0100

    finish reservation ui modal

--- src/src/components/Forms/Booking/Step3/ReservationOwnerPreview.tsx
+++ src/src/components/Forms/Booking/Step3/ReservationOwnerPreview.tsx
@@ -1,4 +1,18 @@
-import { Box, Button, Grid, Hidden, Icon, Typography } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  Grid,
+  Hidden,
+  Icon,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Slide
+} from '@material-ui/core'
+import { TransitionProps } from '@material-ui/core/transitions'
 import {
   MailIcon,
   MobileIcon,
@@ -8,9 +22,24 @@ import {
   UserIcon
 } from '~/src/components/Icons'
 import React from 'react'
-import { FiUsers } from 'react-icons/fi'
-
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="down" ref={ref} {...props} />
+})
 const ReservationOwnerPreview: React.FC = () => {
+  const [isReservationModalOpen, setIsReservationModalOpen] = React.useState(
+    false
+  )
+
+  const handleReservationModalOpen = () => {
+    setIsReservationModalOpen(true)
+  }
+
+  const handleReservationModalClose = () => {
+    setIsReservationModalOpen(false)
+  }
   return (
     <Box mb={5}>
       <Grid container spacing={4} direction="column">
@@ -29,6 +58,7 @@ const ReservationOwnerPreview: React.FC = () => {
                   variant="outlined"
                   fullWidth
                   startIcon={<ScheduleIcon />}
+                  onClick={handleReservationModalOpen}
                 >
                   Reservar
                 </Button>
@@ -80,6 +110,77 @@ const ReservationOwnerPreview: React.FC = () => {
           </Grid>
         </Box>
       </Grid>
+      {/* reservation Dialog */}
+      <Box>
+        <Dialog
+          open={isReservationModalOpen}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleReservationModalClose}
+          aria-labelledby="reservation-dialog"
+          aria-describedby="reservation"
+        >
+          <DialogTitle id="reservation-dialog">
+            RESERVADO COM SUCESSO
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="reservation">
+              <Typography variant="h6" style={{ marginBottom: '1rem' }}>
+                <b> Codigo de Reserva</b> - <i> #uy32789j</i>
+              </Typography>
+
+              <Box>
+                <Typography variant="h6" style={{ marginBottom: '1rem' }}>
+                  <b>Titular da Reserva</b>
+                </Typography>
+                <Grid container spacing={1} md={10}>
+                  <Grid item container alignItems="center" md={6}>
+                    <Icon>
+                      <UserIcon />
+                    </Icon>
+                    <Typography style={{ marginLeft: '1rem' }}>
+                      Jhon Doe
+                    </Typography>
+                  </Grid>
+                  <Grid item container style={{ marginTop: 4 }} md={6}>
+                    <Icon>
+                      <MailIcon />
+                    </Icon>
+                    <Typography style={{ marginLeft: '1rem' }}>
+                      example@example.com
+                    </Typography>
+                  </Grid>
+                  <Grid item container style={{ marginTop: 4 }} md={6}>
+                    <Icon>
+                      <MobileIcon />
+                    </Icon>
+                    <Typography style={{ marginLeft: '1rem' }}>
+                      999 99 99
+                    </Typography>
+                  </Grid>
+                  <Grid item container style={{ marginTop: 4 }} md={6}>
+                    <Icon>
+                      <PhoneIcon />
+                    </Icon>
+                    <Typography style={{ marginLeft: '1rem' }}>
+                      999 99 99
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleReservationModalClose}
+              color="primary"
+              variant="contained"
+            >
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
     </Box>
   )
 }
