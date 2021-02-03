2b67ec639009e826b9fe8ea1664ce16aff3549ac
kid <kidiatoliny@gmail.com>
2021-02-03 13:02:21 -0100

    finish step2 ui

---
+++ src/src/components/Forms/Booking/Step2/PassengerData.tsx
@@ -0,0 +1,141 @@
+import {
+  Box,
+  FormControl,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Typography
+} from '@material-ui/core'
+import { Field } from 'formik'
+import { Select, TextField } from 'formik-material-ui'
+import React from 'react'
+import {
+  AiOutlineClockCircle,
+  AiOutlineFileSearch,
+  AiOutlineUserAdd
+} from 'react-icons/ai'
+import { FiHash, FiPhone, FiSmartphone, FiUsers } from 'react-icons/fi'
+import { HiOutlineHashtag, HiOutlineMail } from 'react-icons/hi'
+import { IoCarSport } from 'react-icons/io5'
+
+const PassengerData: React.FC = () => {
+  return (
+    <Box marginBottom={4}>
+      <Grid container spacing={4} direction="column">
+        <Grid item xs={12}>
+          <Box mt={2}>
+            <Grid item>
+              <Typography variant="h6">Passageiro 1/1</Typography>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid item container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            <Field
+              component={TextField}
+              variant="outlined"
+              label=" Nome"
+              name="first_name"
+              size="small"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <FiUsers />
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Field
+              component={TextField}
+              variant="outlined"
+              label=" Apelido"
+              name="last_name"
+              size="small"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <FiUsers />
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Box>
+        <Box marginTop={3}>
+          <Grid container spacing={1}>
+            <Grid item sm={6} md={4}>
+              <FormControl fullWidth size="small" variant="outlined">
+                <InputLabel htmlFor="destination">
+                  Tipo de Passageiro
+                </InputLabel>
+                <Field
+                  component={Select}
+                  label=" Tipo de Passageiro"
+                  name="destination"
+                  inputProps={{
+                    id: 'destination'
+                  }}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <AiOutlineUserAdd />
+                    </InputAdornment>
+                  }
+                >
+                  <MenuItem value={10}>SA/SV</MenuItem>
+                  <MenuItem value={20}>SV/SA</MenuItem>
+                </Field>
+              </FormControl>
+            </Grid>
+            <Grid item sm={6} md={4}>
+              <FormControl fullWidth size="small" variant="outlined">
+                <InputLabel htmlFor="destination">Tipo documento</InputLabel>
+                <Field
+                  component={Select}
+                  label="Tipo de documento"
+                  name="destination"
+                  inputProps={{
+                    id: 'destination'
+                  }}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <AiOutlineFileSearch />
+                    </InputAdornment>
+                  }
+                >
+                  <MenuItem value={10}>SA/SV</MenuItem>
+                  <MenuItem value={20}>SV/SA</MenuItem>
+                </Field>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Field
+                component={TextField}
+                variant="outlined"
+                label=" Numero de documento"
+                name="last_name"
+                size="small"
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <HiOutlineHashtag />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default PassengerData
---
+++ src/src/components/Forms/Booking/Step2/VehicleData.tsx
@@ -0,0 +1,110 @@
+import {
+  Box,
+  FormControl,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Typography
+} from '@material-ui/core'
+import { Field } from 'formik'
+import { Select, TextField } from 'formik-material-ui'
+import React from 'react'
+import { AiOutlineUserAdd } from 'react-icons/ai'
+import { FiUsers } from 'react-icons/fi'
+import { IoCarSport } from 'react-icons/io5'
+
+const VehicleData: React.FC = () => {
+  return (
+    <Box marginBottom={4}>
+      <Grid container spacing={4} direction="column">
+        <Grid item xs={12}>
+          <Box mt={2}>
+            <Grid item>
+              <Typography variant="h6">Veículo 1/1</Typography>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid item container spacing={4} wrap="wrap">
+          <Grid item xs={12} sm={6}>
+            <Field
+              component={TextField}
+              placeholder="eg. Toyota"
+              variant="outlined"
+              label=" Marca"
+              name="first_name"
+              size="small"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IoCarSport />
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Field
+              component={TextField}
+              variant="outlined"
+              label=" Modelo"
+              placeholder="eg. Hilux"
+              name="last_name"
+              size="small"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IoCarSport />
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Grid>
+          <Grid item sm={6}>
+            <FormControl fullWidth size="small" variant="outlined">
+              <InputLabel htmlFor="destination">Tipo de veículo</InputLabel>
+              <Field
+                component={Select}
+                label=" Tipo de veiculo"
+                name="destination"
+                inputProps={{
+                  id: 'destination'
+                }}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <IoCarSport />
+                  </InputAdornment>
+                }
+              >
+                <MenuItem value={10}>SA/SV</MenuItem>
+                <MenuItem value={20}>SV/SA</MenuItem>
+              </Field>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Field
+              component={TextField}
+              variant="outlined"
+              label=" Matricula"
+              placeholder="xx-xx-xx"
+              name="last_name"
+              size="small"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <FiUsers />
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default VehicleData
