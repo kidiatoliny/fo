e88074f731e643cde921916c65165e58160cc8c0
kid <kidiatoliny@gmail.com>
2021-02-05 17:25:25 -0100

    fix api interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,14 @@
 import axios from 'axios'
+
 const { REACT_APP_API } = process.env
-const api = axios.create({ baseURL: REACT_APP_API })
-api.interceptors.response.use(
-  response => {
-    return response
-  },
-  error => {
-    return Promise.reject(error.response)
-  }
-)
+
+const api = axios.create({
+  baseURL: REACT_APP_API
+})
+
+api.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token')
+  config.headers.Authorization = token ? `Bearer ${token}` : ''
+  return config
+})
 export default api
